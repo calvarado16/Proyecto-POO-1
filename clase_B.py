e4803ac805f15ee385be86ee0a7b1a2d64ec66fc
calvarado16 <caalvaradof04@gmail.com>
2025-06-19 15:54:30 -0600

    clase_B.py

--- clase_B.py
+++ clase_B.py
@@ -14,13 +14,6 @@ class tarjeta_credito:
     numero_tarjeta : int 
     CVV : int
     cuenta_id: str = ""
-
-    def atributosB(self):
-        print("Propietario: ", self.nombre_propietario)
-        print("Año de vencimiento: ", self.año_vencimiento)
-        print("Numero de tarjeta: ", self.numero_tarjeta)
-        print("Card Verification Value: ", self.CVV)
-        print("ID cuenta: ", self.cuenta_id) 
            
     def save(self, coll):
         return str( coll.insert_one( asdict(self) ).inserted_id  )
